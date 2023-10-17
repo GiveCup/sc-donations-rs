d9087ace71b86613920007da9333e1a141741e21
lucasmanea21 <lucasmanea@Yahoo.com>
2023-10-17 10:49:44 +0300

    update

--- simple/src/lib.rs
+++ simple/src/lib.rs
@@ -7,15 +7,20 @@ pub trait Contract {
     #[init]
     fn init(&self) {}
 
+
     #[payable("*")]
     #[endpoint]
     fn donate(&self, organization_id: usize) {
         let payment = self.call_value().egld_value();
+        require(payment > 0, "Payment must be greater than 0");
+
         let caller = self.blockchain().get_caller();
 
         let organization_wallet = self.organizations().get(organization_id);
-        
-        self.send().direct(&organization_wallet, &EgldOrEsdtTokenIdentifier::egld(), 0, &payment);
+        require(organization_wallet.is_some(), "Organization does not exist");
+
+        let organization_balance = self.organization_balance(&organization_wallet);
+        self.organization_balance(&organization_wallet).set(&organization_balance + &payment);
     }
 
     #[only_owner]
@@ -24,7 +29,16 @@ pub trait Contract {
         self.organizations().push(&address);
     }
 
+    #[endpoint(withdraw)]
+    fn withdraw(&self) {
+        let caller = self.blockchain().get_caller();
+        
+    }
+
     #[view(getOrganizations)]
     #[storage_mapper("organizations")]
-    fn organizations(&self) -> VecMapper<ManagedAddress>;
+    fn organizations(&self) -> VecMapper<&ManagedAddress>;
+
+    #[storage_mapper("organizationBalance")]
+    fn organization_balance(&self, address: &ManagedAddress) -> SingleValueMapper<&BigUint>;
 }
