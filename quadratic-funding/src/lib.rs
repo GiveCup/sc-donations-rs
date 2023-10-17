c007cc6aceee683789048846cb094ca71fd9e50d
lucasmanea21 <lucasmanea@Yahoo.com>
2023-10-17 10:49:23 +0300

    added QF mock

--- quadratic-funding/src/lib.rs
+++ quadratic-funding/src/lib.rs
@@ -1,9 +1,57 @@
 #![no_std]
 
 multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
 
 #[multiversx_sc::contract]
-pub trait Contract {
+pub trait QuadraticFundingContract {
+    // Initialize the contract.
     #[init]
     fn init(&self) {}
+
+    // To add a new project.
+    #[endpoint]
+    fn add_project(&self, project_id: ManagedBuffer) {
+        // Logic to add a new project for which people can donate.
+    }
+
+    // Add matching funds to the contract.
+    #[endpoint]
+    fn add_matching_fund(&self, amount: BigUint) {
+        // Logic to add matching funds to the contract.
+    }
+
+    // Allow users to contribute to a project.
+    #[payable("*")]
+    #[endpoint]
+    fn contribute(&self, project_id: ManagedBuffer, user_id: ManagedBuffer, amount: BigUint) {
+        // Logic to track each user's contributions separately.
+    }
+
+    // Calculate the matched fund for a project.
+    #[view]
+    fn calculate_match(&self, project_id: ManagedBuffer) -> BigUint {
+        // Logic to calculate how much matching fund a project will receive.
+        BigUint::zero() // Placeholder return.
+    }
+
+    // Finalize and distribute the funds.
+    #[endpoint]
+    fn distribute_funds(&self) {
+        // Logic to distribute the matching funds to the projects.
+    }
+
+    // Structures and mappers for storage
+    #[storage_mapper("projects")]
+    fn projects(&self) -> MapMapper<ManagedBuffer, ProjectData<Self::Api>>;
+
+    #[storage_mapper("matching_fund")]
+    fn matching_fund(&self) -> SingleValueMapper<BigUint>;
+
+    #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, TypeAbi, Clone)]
+    pub struct ProjectData<M: ManagedTypeApi> {
+        total_contribution: BigUint<M>,
+        contributors: MapMapper<ManagedBuffer, BigUint<M>>,
+    }
 }
+
